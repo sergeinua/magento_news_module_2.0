2133824fdd342f140ff5573006bf32262bbcdfa3
Sergei <sergei.nikonov@yahoo.com>
2016-08-16 23:50:55 +0300

    back-end

---
+++ app/code/Sergei/News/Controller/Adminhtml/Post/Index.php
@@ -0,0 +1,55 @@
+<?php
+namespace Sergei\News\Controller\Adminhtml\Post;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Sergei_News::post');
+        $resultPage->addBreadcrumb(__('News Posts'), __('News Posts'));
+        $resultPage->addBreadcrumb(__('Manage News Posts'), __('Manage News Posts'));
+        $resultPage->getConfig()->getTitle()->prepend(__('News Posts'));
+
+        return $resultPage;
+    }
+
+    /**
+     * Is the user allowed to view the blog post grid.
+     *
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Sergei_News::post');
+    }
+
+
+}
