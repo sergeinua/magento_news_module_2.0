f061affd355d26397f3fdb3fbd9b8d1a821f8bc6
Sergei <sergei.nikonov@yahoo.com>
2016-08-16 21:33:28 +0300

    models & resource models

---
+++ app/code/Sergei/News/Model/ResourceModel/Post/Collection.php
@@ -0,0 +1,21 @@
+<?php
+namespace Sergei\News\Model\ResourceModel\Post;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    /**
+     * @var string
+     */
+    protected $_idFieldName = 'post_id';
+
+    /**
+     * Define resource model
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('Sergei\News\Model\Post', 'Sergei\News\Model\ResourceModel\Post');
+    }
+
+}
